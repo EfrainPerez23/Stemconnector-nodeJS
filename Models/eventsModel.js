094c361740abfcb6fc4c18fba5ff5f87c329b1d6
Efrain Perez <efrain.perez@stemconnector.com>
2017-11-21 19:59:17 -0500

    Change position of the column in event table (locaion, email)

--- Models/eventsModel.js
+++ Models/eventsModel.js
@@ -8,14 +8,14 @@ let eventModel = {
         return db.query('SELECT *  FROM Event WHERE idEvent = ' + id, callback);
     },
     addEvent: function(ev, callback) {
-        return db.query('INSERT INTO `Event` VALUES (?,?,?,?,?,?,?,?,?, ?)', [null, ev.name, ev.description, ev.status, ev.startDate, ev.endDate, ev.Initiative_idInitiative, ev.email, ev.location, ev.imageUrl], callback);
+        return db.query('INSERT INTO `Event` VALUES (?,?,?,?,?,?,?,?,?, ?)', [null, ev.name, ev.description, ev.status, ev.startDate, ev.endDate, ev.Initiative_idInitiative, ev.location, ev.email, ev.imageUrl], callback);
     },
     deleteEvent: function(id, callback) {
         return db.query('DELETE FROM `Event` WHERE idEvent = ' + id, callback);
     },
     UpdateEvent: function(id, ev, callback) {
         console.log(id);
-        return db.query('UPDATE `Event` SET idEvent = null, name = ?, description = ?, status = ?, startDate = ?, endDate = ?, Initiative_idInitiative = ?, email = ?, location = ?, imageUrl = ? WHERE idEvent = ' + id, [ev.name, ev.description, ev.status, ev.startDate, ev.endDate, ev.Initiative_idInitiative, ev.email, ev.location, ev.imageUrl], callback);
+        return db.query('UPDATE `Event` SET idEvent = null, name = ?, description = ?, status = ?, startDate = ?, endDate = ?, Initiative_idInitiative = ?, location = ?, email = ?, imageUrl = ? WHERE idEvent = ' + id, [ev.name, ev.description, ev.status, ev.startDate, ev.endDate, ev.Initiative_idInitiative, ev.email, ev.location, ev.imageUrl], callback);
     },
     ActivitiesEvent: function(id, callback) {
         return db.query('SELECT * FROM Activity WHERE Event_idEvent = ' + id + 'ORDER BY 3 ASC ', callback);
