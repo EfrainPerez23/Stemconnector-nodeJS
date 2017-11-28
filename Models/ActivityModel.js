b6054504705cff0457b71085bf02e69cf1302c2f
Efrain Perez <efrain.perez@stemconnector.com>
2017-11-28 18:57:25 -0500

    Add location field to the response in Activity Model

--- Models/ActivityModel.js
+++ Models/ActivityModel.js
@@ -2,19 +2,19 @@ const db = require('../DataLayer/dbManager');
 
 let activityModel = {
     getActivities: function(callback) {
-        return db.query('SELECT idActivity, Event_idEvent, startTime , endTime ,  `name` ,  `description` FROM Activity', callback);
+        return db.query('SELECT idActivity, Event_idEvent, startTime , endTime ,  `name` ,  `description`, location FROM Activity', callback);
     },
     getActivity: function(id, callback) {
-        return db.query('SELECT idActivity, Event_idEvent, startTime, endTimE, name, description FROM `Activity` WHERE idActivity = ' + id, callback);
+        return db.query('SELECT idActivity, Event_idEvent, startTime, endTimE, name, description, location FROM `Activity` WHERE idActivity = ' + id, callback);
     },
     addActivity: function(ev, callback) {
-        return db.query('INSERT INTO `Activity` VALUES (?,?,?,?,?,?)', [null, ev.Event_idEvent, ev.startTime, ev.endTime, ev.name, ev.description], callback);
+        return db.query('INSERT INTO `Activity` VALUES (?,?,?,?,?,?,?)', [null, ev.Event_idEvent, ev.startTime, ev.endTime, ev.name, ev.description, ev.location], callback);
     },
     deleteActivity: function(id, callback) {
         return db.query('DELETE FROM `Activity` WHERE idActivity = ' + id, callback);
     },
     UpdateActivity: function(id, ev, callback) {
-        return db.query('UPDATE `Activity` SET Event_idEvent = ?, startTime = ?, endTime = ?, name = ?, description = ? WHERE idActivity = ' + id + ';', [ev.Event_idEvent, ev.startTime, ev.endTime, ev.name, ev.description], callback);
+        return db.query('UPDATE `Activity` SET Event_idEvent = ?, startTime = ?, endTime = ?, name = ?, description = ?, location = ? WHERE idActivity = ' + id + ';', [ev.Event_idEvent, ev.startTime, ev.endTime, ev.name, ev.description], callback);
     }
 };
 
